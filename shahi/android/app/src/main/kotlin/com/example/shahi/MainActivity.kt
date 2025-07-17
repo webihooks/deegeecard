cff1a3622b0587c02e84adbe634ca21c57a81374
Sagar <webihooks@gmail.com>
2025-07-17 19:05:20 +0530

    Android Apk App Main File

---
+++ shahi/android/app/src/main/kotlin/com/example/shahi/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.shahi
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ shahi/lib/main.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Shahi Restaurant',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const WebViewScreen(),
+    );
+  }
+}
+
+class WebViewScreen extends StatefulWidget {
+  const WebViewScreen({super.key});
+
+  @override
+  State<WebViewScreen> createState() => _WebViewScreenState();
+}
+
+class _WebViewScreenState extends State<WebViewScreen> {
+  late final WebViewController _controller;
+  bool _isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onProgress: (int progress) {
+            debugPrint('Loading: $progress%');
+          },
+          onPageStarted: (String url) {
+            setState(() => _isLoading = true);
+            debugPrint('Page started loading: $url');
+          },
+          onPageFinished: (String url) {
+            setState(() => _isLoading = false);
+            debugPrint('Page finished loading: $url');
+          },
+          onNavigationRequest: (NavigationRequest request) {
+            // Handle external links
+            if (!request.url.startsWith('https://deegeecard.com')) {
+              launchUrl(Uri.parse(request.url));
+              return NavigationDecision.prevent;
+            }
+            return NavigationDecision.navigate;
+          },
+        ),
+      )
+      ..loadRequest(Uri.parse('https://deegeecard.com/shahi'));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        toolbarHeight: 0,
+        systemOverlayStyle: SystemUiOverlayStyle(
+          statusBarColor: Colors.transparent,
+          statusBarIconBrightness: Brightness.dark,
+        ),
+      ),
+      body: Stack(
+        children: [
+          Container(
+            margin: EdgeInsets.only(top: MediaQuery.of(context).padding.top),
+            child: WebViewWidget(controller: _controller),
+          ),
+          if (_isLoading)
+            const Center(
+              child: CircularProgressIndicator(),
+            ),
+        ],
+      ),
+    );
+  }
+}
